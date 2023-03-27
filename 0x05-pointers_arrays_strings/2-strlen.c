ed2720eeaa66e46a6eafd543c42f07d37dfcb077
jobmatanda <jobmatanda@gmail.com>
2023-03-27 18:47:31 +0000

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,17 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluate
+ * @job: string to evaluate
  *
  * Return: the length of the string
  */
-int _strlen(char *s)
+int _strlen(char *job)
 {
 	int i;
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (job[i] != '\0')
 	{
 		i++;
 	}
