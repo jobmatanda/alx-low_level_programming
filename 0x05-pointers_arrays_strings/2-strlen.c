8a516f891e15753650ffcaa7f497c1d474a815f4
jobmatanda <jobmatanda@gmail.com>
2023-03-27 18:40:18 +0000

    mm

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * _strncpy - this fuction will concatenates two strings
- * @dest: the string that will modified.
- * @src: the string that ill be concatenated to @des.
- * @n: how much to copy from src into dest.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: a pointer to @des
+ * Return: the length of the string
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strlen(char *s)
 {
-	int i_01 = 0;
+	int i;
 
-	for (i_01 = 0; i_01 < n && src[i_01] != '\0'; i_01++)
-		dest[i_01] = src[i_01];
-	for ( ; i_01 < n ; i_01++)
-		dest[i_01] = '\0';
+	i = 0;
 
-	return (dest);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
-
