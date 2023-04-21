830264c037e00a8cf519de7aa108bb1e0f1d6e98
jobmatanda <jobmatanda@gmail.com>
2023-04-21 02:48:36 +0000

    4-new_dog.c Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,6 +6,7 @@
  * @s: string to evaluate
  *
  * Return: the length of the string
+ * Mabujo work
  */
 int _strlen(char *s)
 {
@@ -63,20 +64,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-	l1 = _strlen(name);
-	l2 = _strlen(owner);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (l1 + 1));
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (l2 + 1));
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
